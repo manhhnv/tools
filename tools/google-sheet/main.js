0ef310766c1c70a3e24c91bc207e889c667074f6
manhnv <manh117bg@gmail.com>
2021-12-10 23:18:31 +0700

    update sheet tools

--- tools/google-sheet/main.js
+++ tools/google-sheet/main.js
@@ -15,7 +15,45 @@ const main = async () => {
     .filter((e) => e.isDirectory());
 
   const listNames = dirs.map((e) => e.name);
+  listNames.forEach(async (e, i) => {
+    const directory = path.resolve(process.cwd(), "data", e);
+    const files = walkFiles(directory);
+    const rawData = fs.readFileSync(files[0]);
+    const data = JSON.parse(rawData);
+    const sheetData = [["word_id", "content", "meaning", "en", "vi"]];
+    for (const item of data.items) {
+      item.sentences.forEach((sentence) => {
+        sheetData.push([
+          item._id,
+          item.content,
+          item.meaning,
+          sentence.en,
+          sentence.vi,
+        ]);
+      });
+    }
+    await googlSheet.clearAll(e);
+    await googlSheet.writeAll(e, sheetData);
+    console.log(`${i}/${listNames.length}`);
+  });
+};
 
+const walkFiles = (directory) => {
+  const results = [];
+  const files = fs.readdirSync(directory, {
+    encoding: "utf-8",
+    withFileTypes: true,
+  });
+  files.forEach((file) => {
+    const statPath = path.resolve(directory, file.name);
+    if (file.isDirectory()) {
+      const res = walkFiles(statPath);
+      results.push(...res);
+    } else if (file.isFile()) {
+      results.push(statPath);
+    }
+  });
+  return results;
 };
 
 main().catch((e) => {
